b3bda671f441b3fd8714053bc5b5c3c53f6ffba4
Jude Clarke <judepaclarke@gmail.com>
2025-03-04 20:19:11 -0500

    add styling to form and create button component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./form-input.styles.css";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input {...otherProps} />
+    <div className="group">
+      <input className="form-input" {...otherProps} />
+
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
     </div>
   );
 };
